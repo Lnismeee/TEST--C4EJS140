d9a3650a23739157078d9d32bd3fd9db277f5a87
Lnismeee <86096138+Lnismeee@users.noreply.github.com>
2023-07-13 23:05:28 +0700

    FIX

--- bai2/index.js
+++ bai2/index.js
@@ -1,8 +1,13 @@
+let container = document.querySelector(".in");
+
 function numberOneTriangle(x) {
     let a = "";
     for (let i = 1; i <= x; i++) {
         a += "*";
-        console.log(a);
+        const element = document.createElement("div");
+        element.innerText = a;
+        container.appendChild(element);
     }
 }
-numberOneTriangle(3);
+
+numberOneTriangle(5);
