e43a443b58ae3220e727fcb2333040b432f21ac4
Lnismeee <86096138+Lnismeee@users.noreply.github.com>
2023-07-13 22:39:34 +0700

    First commit

---
+++ bai1/index.js
@@ -0,0 +1,25 @@
+let a, b;
+a = prompt("Nhập số bắt đầu của dãy số (a): ");
+do {
+    b = prompt("Nhập số kết thúc của dãy số (b) (b > a):");
+} while (a > b);
+a = new Number(a);
+b = new Number(b);
+
+let sum = 0;
+
+for (let i = a; i <= b; i++) {
+    let s = 0;
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            s = 1;
+            break;
+        }
+    }
+    if (i > 1 && s == 0) {
+        sum += i;
+    }
+}
+console.log(
+    "Tổng số nguyên tố của chuỗi từ " + a + " đến " + b + " là: " + sum
+);
---
+++ bai2/index.js
@@ -0,0 +1,8 @@
+function numberOneTriangle(x) {
+    let a = "";
+    for (let i = 1; i <= x; i++) {
+        a += "*";
+        console.log(a);
+    }
+}
+numberOneTriangle(3);
